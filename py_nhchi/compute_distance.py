ecd72dae5fee4d67663efc0d51cddc2f71684f0d
Nan-Hsun Chi <nanhsunchi@gmail.com>
2024-09-09 15:59:35 -0700

    modify compute_distance.py, created windstr.py

--- py_nhchi/compute_distance.py
+++ py_nhchi/compute_distance.py
@@ -8,16 +8,22 @@ def distance(lat1,lon1,lat2,lon2):
     # The math module contains a function named
     # radians which converts from degrees to radians.
     lon1 = math.radians(lon1)
-    lon2 = math.radians(lon2)
     lat1 = math.radians(lat1)
+    lon2 = math.radians(lon2)
     lat2 = math.radians(lat2)
 
     # Haversine formula
     dlon = lon2 - lon1 
     dlat = lat2 - lat1
-    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.acos
-    c = 2 * math.asin(math.sqrt(a)) 
+    a = math.sin( dlat/2 )**2 + math.cos(lat1) * math.cos(lat2) * math.sin( dlon/2 )**2
+    c = 2 * math.asin( math.sqrt(a) ) 
     # Radius of earth in kilometers. Use 3956 for miles
     r = 6371
      # calculate the result
     return(c * r)
+
+# lat1 = 53.32055555555556
+# lat2 = 53.31861111111111
+# lon1 = -1.7297222222222221
+# lon2 =  -1.6997222222222223
+# print(distance(lat1, lon1, lat2, lon2), "K.M")
---
+++ py_nhchi/windstr.py
@@ -0,0 +1,51 @@
+import numpy as np
+import math
+
+def ra_windstr_nc(u,v,roh):
+    ### This is translated from ra_windstr_nc.m, which was modified from ra_windstr.m written by 
+    # # Ramkrushn S. Patel (ramkrushn.scrv89@gmail.com)
+
+    # DESCRIPTION:  Function to compute wind stress from wind field data Based on Gill, 1982 
+    # Formula and a non-linear Cd based on Large and Pond (1981), modified for low wind 
+    # speeds (Trenberth et al., 1990)
+    # 
+    # INPUTS: 
+    # u = Zonal wind component [m/s], must be 2D
+    # v = Meridional wind component [m/s], must be 2D
+    #
+    # OUTPUT: 
+    # Tx = Zonal wind stress [N/m^2]
+    # Ty = Meridional wind stress [N/m^2]
+    # 
+    # DISCLAIMER: 
+    # Albeit this function is designed only for academic purpose, it can be implemented in 
+    # research. Nonetheless, author does not guarantee the accuracy.
+    # 
+    # REFERENCE:
+    # A.E. Gill, 1982, �Atmosphere-Ocean Dynamics�, Academy Press, Vol. 30.
+    # W. G. Large & S. Pond., 1981,�Open Ocean Measurements in Moderate to Strong Winds�, 
+    # J. Physical Oceanography, Vol. 11, pp. 324 - 336.
+    # K.E. Trenberth, W.G. Large & J.G. Olson, 1990, �The Mean Annual Cycle in Global Ocean 
+    # Wind Stress�, J.Physical Oceanography, Vol. 20, pp. 1742 � 1760.
+
+    # Defining Constant 
+    # roh=1.2; % kg/m^3, air density
+    # Computation of Wind Stresses
+    lt, ln = u.shape
+    Tx = np.nan*np.ones( (lt, ln) )
+    Ty = Tx.copy()
+
+    for ii in range(lt):
+        for jj in range(ln):
+            U = np.sqrt( np.square(u[ii,jj]) + np.square(v[ii,jj])) # Wind speed
+            if U <= 1:
+                  Cd=0.00218
+            elif (U > 1) | (U <= 3):
+                 Cd=(0.62+1.56/U)*0.001
+            elif (U > 3) | (U < 10):
+                 Cd=0.00114; 
+            else:
+                 Cd=(0.49+0.065*U)*0.001;
+            
+            Tx[ii, jj]=Cd*roh*U*u[ii, jj]# kg/m^3*m/s*m/s= N/m^2
+            Ty[ii, jj]=Cd*roh*U*v[ii, jj]
