811bf5fe22cfc79a04d7d0a904790a4ea1914c26
Nan-Hsun Chi <nanhsunchi@gmail.com>
2025-03-19 15:14:08 -0700

    add Cd_e13_CH2020(U10, V10) - which combines Edson et al. (2013) & Curcic and Haus (2020)'s Cd values and interpolate them

--- py_nhchi/windstr.py
+++ py_nhchi/windstr.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import netCDF4 as nc 
 
 def ra_windstr_nc(u,v,roh):
     ### This is translated from ra_windstr_nc.m, which was modified from ra_windstr.m written by 
@@ -83,4 +84,43 @@ def wind_adj2height_PL(u1, z1, z2):
      z2_z1 = np.divide( z2, z1 )
      u2 = np.multiply(u1, np.power(z2_z1,0.11))
      return u2
+
+def Cd_e13_CH2020(U10,V10):
+     ''' get Cd(10m) values for U10/V(10) 
+     input: U10, V10: zonal and meridional wind velocity at 10m in m/s.
+     output: Cd value at 10m at the input wind velocity. 
+     '''
+     ''' Look at input data '''
+     if isinstance(U10, np.ndarray) | isinstance(U10, list) \
+          | isinstance(U10, float) | isinstance(U10, int):
+          U10M = np.sqrt( np.square(U10) + np.square(V10) )
+     else:
+          print('Input data type has to the following: np.ndarray, list, float, int.')
+          return
+     ''' READ Cd Data '''
+     ''' Read Edson et al. (2013), field data '''
+     path = '/Users/chi/Documents/Data/Curcic-Haus-2020-1.0.0/data/'
+     e13 = np.loadtxt(path+'edson_etal_2013.txt', skiprows=1)
+     U10_e13, CD_e13, CD_std_e13 = e13[:,0], e13[:,1]*1e-3, e13[:,2]*1e-3
+     ''' Read Curcic & Haus (2020), lab data '''
+     CH2020 = nc.Dataset( path+'asist-windonly-salt.nc' )
+     U10_CH2020 = CH2020.variables['U10_momentum_budget'][:]
+     CD_CH2020 = CH2020.variables['CD_momentum_budget'][:]
+     # remove high-error momentum budget estimates
+     CD_CH2020[:6] = np.nan
+     # print(U10_e13)
+     # print(CD_e13)
+     # print(U10_CH2020)
+     # print(CD_CH2020)
+     ''' Combine Cd Data '''
+     iCH2020 = U10_CH2020> U10_e13[-1]
+     U10_combine = np.concatenate( (U10_e13, U10_CH2020[iCH2020]),axis=0 )
+     CD10_combine = np.concatenate( (CD_e13, CD_CH2020[iCH2020]),axis=0 )
+     # print(U10_combine)
+     # print(CD10_combine)
+     ''' interpolate to get Cd values '''
+     CD10_interp = np.interp( U10M, U10_combine, CD10_combine, \
+                             left=CD10_combine[0], right=CD10_combine[-1] )
+     return CD10_interp
+
      
