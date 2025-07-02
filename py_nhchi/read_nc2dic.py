52bc088d3bff143b9d2a6d36eb5dbf273d8b50e1
Nan-Hsun Chi <nanhsunchi@gmail.com>
2025-07-02 10:47:32 -0700

    modified--> the output array type of data in the dictionary is np ndarray

--- py_nhchi/read_nc2dic.py
+++ py_nhchi/read_nc2dic.py
@@ -14,45 +14,52 @@ def load_nc2dic(filename, sticker, basetime, timedelta):
     dic = {}
     ds = nc.Dataset(filename)
     keys = list( ds.variables.keys() )
+    print(keys)
     ''' put data to dictionary '''
     for key in keys:
-        data_ma = np.squeeze( ds.variables[key][:] )
-        fillvalue = data_ma.get_fill_value()
-        shape = data_ma.shape
-        if len(shape) == 0:
-            print(key,'shape is (). Will skip.')
-        else:
-            print(key, shape, 'fillvalue=',fillvalue)
-            # np.ma.set_fill_value( data_ma, np.nan )
-            # data = np.squeeze( np.ma.getdata( data_ma ) )
-            # ifill = np.where( (data_ma== fillvalue) | (np.abs(data_ma>=1e20)) )[0]
-            ifill = (np.ma.getmask(data_ma)) | (np.abs(data_ma>= 1e20))
-            data = data_ma
-            data[ifill] = np.nan
-            ''' make new variable names (1) lower cases  
+        ''' make new variable names (1) lower cases  
                                         (3) vel_east to u, (4) vel_north to v
                                         (5) lonXXX to longitude (5) latXXX to latitude '''
-            var = key.lower()
-            if var == 'vel_east':
-                var = 'u'
-            if var == 'vel_north':
-                var = 'v'
-            if 'lon' in var:
-                var = 'longitude'
-            if 'lat' in var:
-                var = 'latitude'
-            # i1 = var.find('_')
-            # if i1> 0:
-            #     var = var[:i1]
-            if sticker != '':
-                var = sticker+'-'+var
-            # print('variable name= '+var)
-            dic[var] = data
-            ''' add datetime to dictionary '''
-            if 'time' in key.lower():
-                dtime = np.array([basetime+item*timedelta for item in dic[var]])
-                dic[sticker+'-dtime'] = dtime
-                print('dtime rangen:',np.min(dtime),np.max(dtime))
+        var = key.lower()
+        if var == 'vel_east':
+            var = 'u'
+        if var == 'vel_north':
+            var = 'v'
+        if 'lon' in var:
+            var = 'longitude'
+        if 'lat' in var:
+            var = 'latitude'
+        # i1 = var.find('_')
+        # if i1> 0:
+        #     var = var[:i1]
+        if sticker != '':
+            var = sticker+'-'+var
+        ''' Read data '''
+        data = ds.variables[key]
+        if data.dtype == str:
+           print(key, 'is str')
+           dic[var] = data
+        else: # not string
+            data_ma = np.squeeze( ds.variables[key][:] )
+            if isinstance(data_ma,np.ndarray): # not string, but array
+                data_ma = np.ma.array( np.squeeze( ds.variables[key][:] ) ).astype(float)
+                np.ma.set_fill_value(data_ma, np.nan)
+                fillvalue = data_ma.get_fill_value()
+                shape = data_ma.shape
+                print(key, shape, data_ma.dtype, 'fillvalue=',fillvalue)
+                ifill = (np.ma.getmask(data_ma)) | (np.abs(data_ma>= 1e20))
+                data = data_ma
+                data[ifill] = np.nan
+                data = np.ma.getdata( data )
+                dic[var] = data
+                ''' add datetime to dictionary '''
+                if 'time' in key.lower():
+                    dtime = np.array([basetime+item*timedelta for item in dic[var]])
+                    dic[sticker+'-dtime'] = dtime
+                    print('dtime rangen:',np.min(dtime),np.max(dtime))
+            else: # not string nor array
+                print(key,'not array')
+                dic[var] = data_ma
     print('load '+filename+' to dictionary')
     print('variables in dictionary:',list(dic.keys()))
     return dic
